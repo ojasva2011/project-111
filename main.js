b80caa2cc31ec3d70932e40dac53d37ae8fafb5b
ojasva2011 <90315077+ojasva2011@users.noreply.github.com>
2022-05-15 09:58:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+prediction="";
+
+Webcam.set({
+width:350,
+height:300,
+image_format:'png',
+png_quality:90
+});
+
+camera=document.getElementById("camera");
+Webcam.attach('#camera');
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+    document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'">';
+    });
+}
+console.log("ml5 version"+ml5.version);
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/ybknGSsRJ/model.json',modelLoaded);
+
+function modelLoaded(){
+    console.log("modelLoaded")
+}
+
+function speak(){
+    var synth=window.speechSynthesis;
+    speak_data="The Prediction Is "+prediction;
+       var utterThis=new speakSynthesisUtterence(speak_data);
+    synth.speak(utterThis);
+
+}
